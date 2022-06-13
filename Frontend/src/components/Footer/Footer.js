93d4ed610a7fe469bccada992629d6e07ad7a80a
iyukukuku <nurruddinafif16@gmail.com>
2022-06-13 08:11:47 +0700

    Update Footer.js

--- Frontend/src/components/Footer/Footer.js
+++ Frontend/src/components/Footer/Footer.js
@@ -10,7 +10,7 @@ const Footer = () => {
           <MDBCol md="3" lg="3" xl="4" className="mx-auto mt-3">
             <h6 className="text-uppercase mb-4 font-weight-bold">
             <img src={logo} alt="Book Store App" height="50px" />
-               <strong>Book-IT</strong>              
+               <strong>UsedBooks</strong>              
             </h6>
             <p>
             "Toko Buku Pendidikan Bekas Berkualitas Dengan Harga Terjangkau
